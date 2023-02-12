e397eedd443e26b1d6ab978fa5a64326b4b0d0ef
Md Wasim Aslam <wasimaslam2897@gmail.com>
2023-02-13 00:31:41 +0530

    #

--- src/App.js
+++ src/App.js
@@ -1,15 +1,55 @@
 import React from "react";
-import Button from '@mui/material/Button';
-import SettingsIcon from '@mui/icons-material/Settings';
-import AddIcon from '@mui/icons-material/Add';
-
+import Button from "@mui/material/Button";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AddIcon from "@mui/icons-material/Add";
+import { styled, Typography } from "@mui/material";
+import { margin } from "@mui/system";
 
 function App() {
+  const BlueBtn = styled(Button)({
+    backgroundColor: "blue",
+    color: "white",
+    margin: "5",
+    "&:hover": {
+      backgroundColor: "gray",
+      color: "black",
+    },
+  });
+
   return (
     <>
       <Button variant="text">Text</Button>
-      <Button variant="contained" color="secondary" size="small" startIcon={<SettingsIcon/>}>Contained</Button>
-      <Button variant="contained" color="success" startIcon={<AddIcon/>} >Outlined</Button>
+      <Button
+        variant="contained"
+        color="secondary"
+        size="small"
+        startIcon={<SettingsIcon />}
+      >
+        Contained
+      </Button>
+      <Button variant="contained" color="success" startIcon={<AddIcon />}>
+        Outlined
+      </Button>
+
+      <Typography variant="h1" component="h2">
+        I am mui H1 Tag
+      </Typography>
+
+      <Button
+        sx={{
+          backgroundColor: "salmon",
+          color: "#888",
+          margin: "5",
+          "&:hover": {
+            backgroundColor: "gray",
+            color: "black",
+          },
+        }}
+      >
+        My Unique Button
+      </Button>
+
+      <BlueBtn>My Custom Btn</BlueBtn>
     </>
   );
 }
